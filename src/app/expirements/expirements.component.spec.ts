49be0f169fa81dda2ccde04e7c6202f7972db3e0
Kaz <kikainvr@gmail.com>
2022-08-01 19:22:58 -0500

    Finished

---
+++ src/app/expirements/expirements.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExpirementsComponent } from './expirements.component';
+
+describe('ExpirementsComponent', () => {
+  let component: ExpirementsComponent;
+  let fixture: ComponentFixture<ExpirementsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ExpirementsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ExpirementsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
